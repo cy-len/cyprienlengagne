0ee94a59c32a38c8c2b0fa25bf378ced7261ec33
cy-len <cyprien.lengagne@gmail.com>
2025-01-05 11:09:40 +0100

    opengraph oopsies 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,8 +17,8 @@
     <meta property="og:type" content="website" />
     <meta property="description" content={$openGaphProps.description ?? ""} />
     <meta property="og:description" content={$openGaphProps.description ?? ""} />
-    <meta property="og:image" content={$openGaphProps.imageUrl ?? "https://cyprienlengagne.com/Valere_Top.webp"} />
-    <meta property="og:image:secure_url" content={$openGaphProps.imageUrl ?? "https://cyprienlengagne.com/Valere_Top.webp"} />
+    <meta property="og:image" content={$openGaphProps.imageUrl ?? "https://cyprienlengagne.com/imgs/Valere_Top.webp"} />
+    <meta property="og:image:secure_url" content={$openGaphProps.imageUrl ?? "https://cyprienlengagne.com/imgs/Valere_Top.webp"} />
 </svelte:head>
 
 <slot />
